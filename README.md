bd76589ee353cfaaf8629f2b1b7097924a1d5163
jocelo <gopher.ae@gmail.com>
2015-11-30 22:13:02 -0600

    version 0.1.11

--- README.md
+++ README.md
@@ -1,4 +1,127 @@
 # offset-dates
 js library to play with dates
 
-Library to handle date operations with ease.
+Lightweigth library to calculate different dates and ranges.
+
+Current version 0.1.11
+
+To start using the offsetdate library you just need to include the js file into your code.
+
+```html
+<script src="offsetdates.min.js"></script>
+```
+
+You don't have to initialize the library, once the js file is included the object is ready to be used.
+You can (and it's strongly recommended that you should) chain functions... even when you can do this:
+
+```javascript
+var date1 = od();
+date1.range();
+date1.offset(-2,'weeks');
+date1.output();
+```
+
+you should do this instead:
+
+```javascript
+od().range('week').offset(-2, 'weeks').output();
+```
+
+The offsetdates methods (od from now on) can be divided into sections:
+
+(1) initialization | (2) operations | (3) formatting | (4) special functions
+
+### 1) Initialization
+
+#### single
+default usage, one date will be created
+usage od.single();
+no parameters
+returns: none
+
+examples:
+
+| Code        | Output |
+| ----------- | ------ |
+| od.single() |        |
+
+#### range
+You can calculate dates or ranges of dates. two dates will be created: startDate and endDate
+
+usage range([period])
+where
+    period: week|month|year|quarter
+returns: none
+
+example:
+| Code        | Output |
+| ----------- | ------ |
+| od.range()  |        |
+| od.range('week') |        |
+
+od()
+od().range()
+od().range('week')
+
+### 2) Operations
+
+#### offset
+offset an specific period of time to the calculated dates
+usage: .offset(amount,period)
+where:
+    amount: Integer number for the jump
+    period: string representation of period, can be weeks|months|years|periods
+returns: none
+
+examples:
+od.range('week').offset(1,'week').output()
+od.range('week').offset(-1,'week').output()
+
+#### beginOfDay
+set the time 0:0:00 0000 (hours, minutes, seconds and miliseconds to the specified date, the parameter can be used to set the begin of date to the endDate, for when you are using range
+usage: beginOfDay([useEndDate])
+where:
+    endDate: if specified, the time 0:0:00 will be set to the end date (when range used)
+returns: none
+
+#### endOfDay
+set the time 23:59:59 99 (hours, minutes, seconds and miliseconds to the specified date, the parameter can be used to set the begin of date to the endDate, for when you are using range
+usage: endOfDay([useEndDate])
+where:
+    endDate: if specified, the time 0:0:00 will be set to the end date (when range used)
+returns: none
+
+### 3) Formatting
+#### output
+Returns the calculated dates, if no parameter is used, the method will return the date objects, string formatted otherwise
+usage output([format])
+where
+    format can be an standar js date format 
+returns: none
+
+examples:
+od.output()
+od.simple().output
+od.range('week').output()
+od.range('week').output(1)
+
+### 4) Special functions:
+These methods should be used directly, no configuration needed.
+
+#### getHolidays
+get an array with the holidays for the specified location
+usage: getHolidays([country])
+where:
+    country could be us|mx
+by default, the us holidays will be returned
+returns: array with date objects, from holidays within specified year
+( can be used with offset, to retrieve holidays from a specific year )
+
+### TODO
+In future commits, the following items will be included:
+* Custom date for library initialization
+* Fiscal Week of current week
+* First day of month
+* Last day of month
+* Mutiple dates array results, to be used in select
+* Test cases
--- offsetDates.js
+++ offsetDates.js
@@ -1,14 +1,24 @@
+// offsetdates.js
+// js library to play with dates
+// version - 0.1.11
+// author - Alfredo Alonso
+// license - MIT
 gsDate = (function() {
     var _currDate,
         _startDate,
         _endDate,
         _range,
         _userFormat = false,
+        _holidays,
         init = function() {
             _currDate = new Date();
-            _startDate = _currDate;
-            _endDate = _currDate;
+            _startDate = new Date();
+            _endDate = new Date();
             _range = false;
+            _holidays = {
+                us: [ [0, 1],[0, 19],[1, 16],[4, 25],[6, 4],[8, 7],[9, 12],[10, 11],[10, 26],[11, 25] ],
+                mx: [ [0, 1],[1, 2],[1, 5],[2, 16],[2, 21],[4, 1],[8, 16],[10, 16],[10, 20],[11, 25] ]
+            };
         },
         getDate = function() {
             return _currDate.toString();
@@ -25,33 +35,98 @@ gsDate = (function() {
         _setCustomDate = function( customDate ) {
             return this;
         },
-        setRange = function() {
-            _startDate = _currDate;
-            _endDate = _currDate;
+        setRange = function( period ) {
+            var mondayDaysOffset = [6,0,1,2,3,4,5],
+                sundayDaysOffset = [0,6,5,4,3,2,1],
+                startTimeStamp = _startDate.valueOf(),
+                endTimeStamp = _endDate.valueOf(),
+                daysToSubstract = mondayDaysOffset[_startDate.getDay()],
+                daysToAdd = sundayDaysOffset[_endDate.getDay()];
+
+            switch(period) {
+                case 'week':
+                    _startDate.setTime( startTimeStamp - daysToSubstract*86400000 );
+                    _endDate.setTime( endTimeStamp + daysToAdd*86400000 );
+                    break;
+                case 'month':
+                    _startDate = new Date(_startDate.getFullYear(), _startDate.getMonth(), 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+                    _endDate = new Date(_endDate.getFullYear(), _endDate.getMonth()+1, 0, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+                    break;
+                case 'year':
+                    _startDate = new Date(_startDate.getFullYear(), 1, 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+                    _endDate = new Date(_endDate.getFullYear(), 11, 31, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+                    break;
+                case 'yeartodate':
+                    _startDate = new Date(_startDate.getFullYear(), 0, 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+                    _endDate = new Date(_endDate.getFullYear(), _endDate.getMonth(), _endDate.getDate(), _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+                    break;
+                case 'quarter':
+                    var quarterMonth = Math.floor(_startDate.getMonth()/3) * 3;
+                    _startDate = new Date(_startDate.getFullYear(), quarterMonth, 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+                    _endDate = new Date(_endDate.getFullYear(), quarterMonth+3, 0, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+                    // _startDate = new Date(_startDate.getFullYear(), 0, 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+                    // _endDate = new Date(_endDate.getFullYear(), _endDate.getMonth(), _endDate.getDate(), _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+                    break;
+                default:
+                    _startDate = new Date();
+                    _endDate = new Date();
+                    break;
+            }
+
             _range = true;
             return this;
         },
         setSingle = function() {
-            _theDate = _currDate;
+            _theDate = new Date();
             _range = false;
             return this;
         },
+        bod = function( dateFlag ) {
+            if (typeof(dateFlag) == 'undefined' || dateFlag == 'start') {
+                _startDate.setHours(0);
+                _startDate.setMinutes(0);
+                _startDate.setSeconds(0);
+                _startDate.setMilliseconds(0);
+            } else if ( dateFlag == 'end' ) {
+                _endDate.setHours(0);
+                _endDate.setMinutes(0);
+                _endDate.setSeconds(0);
+                _endDate.setMilliseconds(0);
+            }
+
+            return this;
+        },
+        eod = function( dateFlag ) {
+            if (typeof(dateFlag) == 'undefined' || dateFlag == 'start') {
+                _startDate.setHours(23);
+                _startDate.setMinutes(59);
+                _startDate.setSeconds(59);
+                _startDate.setMilliseconds(99);
+            } else if ( dateFlag == 'end' ) {
+                _endDate.setHours(23);
+                _endDate.setMinutes(59);
+                _endDate.setSeconds(59);
+                _endDate.setMilliseconds(99);
+            }
+
+            return this;
+        },
         returnFnc = function( format ) {
-            console.log('---------------------------');
             var goAndFly = '',
                 months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
                 shortMonths = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
             
-            console.log('format',format);
             if ( format ) {
-                _startDate = _startDate.getDate()+'-'+shortMonths[_startDate.getMonth()]+'-'+_startDate.getFullYear();
-                _endDate = _endDate.getDate()+'-'+shortMonths[_endDate.getMonth()]+'-'+_endDate.getFullYear();
+                _outStartDate = _startDate.getDate()+'-'+shortMonths[_startDate.getMonth()]+'-'+_startDate.getFullYear();
+                _outEndDate = _endDate.getDate()+'-'+shortMonths[_endDate.getMonth()]+'-'+_endDate.getFullYear();
+            } else {
+                _outStartDate = _startDate;
+                _outEndDate = _endDate;
             }
 
             goAndFly = _startDate;
-            console.log('range',_range);
             if ( _range ) {
-                goAndFly = {startDate:_startDate, endDate:_endDate};
+                goAndFly = {startDate:_outStartDate, endDate:_outEndDate};
             }
 
             return goAndFly;
@@ -79,8 +154,10 @@ gsDate = (function() {
                     _offddMonth(ammount);
                     break;
                 case 'years':
+                    _offddYear(ammount);
                     break;
                 case 'quarters':
+                    _offddQuarter(ammount);
                     break;
                 default:
                     break;
@@ -90,54 +167,17 @@ gsDate = (function() {
         /*
         / Weeks
         */
-        beginOfWeek = function() {
-            
-        },
         _offddWeek = function( weeksOffset ) {
-            console.log("startDate",_startDate);
-            console.log("endDate",_endDate);
-
-            var tmpStartDate = new Date(),
-                tmpTimeStamp = _startDate.valueOf();
-            tmpStartDate.setTime( tmpTimeStamp + 7*weeksOffset*86400000 );
-            _startDate = tmpStartDate;
-
-            tmpTimeStamp = _endDate.valueOf();
-            tmpStartDate.setTime( tmpTimeStamp + 7*weeksOffset*86400000 );
-            _endDate = tmpStartDate;
-
+            _startDate.setTime( _startDate.valueOf() + 7*Number(weeksOffset)*86400000 );
+            _endDate.setTime( _endDate.valueOf() + 7*Number(weeksOffset)*86400000 );
             return this;
-
-        //     var dateOne = new Date(),
-        //         dateTwo = new Date(),
-        //         mondayDaysOffset = [6,0,1,2,3,4,5],
-        //         sundayDaysOffset = [0,6,5,4,3,2,1],
-        //         tmpTimeStamp = _currDate.valueOf(),
-        //         daysToSubstract = mondayDaysOffset[_currDate.getDay()],
-        //         daysToAdd = sundayDaysOffset[_currDate.getDay()];
-
-        //     dateOne.setTime( tmpTimeStamp - daysToSubstract*86400000 + 7*weeksOffset*86400000 );
-        //     dateTwo.setTime( tmpTimeStamp + daysToAdd*86400000 + 7*weeksOffset*86400000);
-        //     dateOne.setHours(0);
-        //     dateOne.setMinutes(0);
-        //     dateOne.setSeconds(0);
-        //     dateTwo.setHours(23);
-        //     dateTwo.setMinutes(59);
-        //     dateTwo.setSeconds(59);
-
-        //     _startDate = dateOne;
-        //     _endDate = dateTwo;
-        //     return this;
         },
         /*
         / Months
         */
         _offddMonth = function( monthsOffset ) {
-            if (monthsOffset == undefined || typeof monthsOffset != 'number') {
-                monthsOffset = 0;
-            }
-            _startDate = new Date(_currDate.getFullYear(), _currDate.getMonth()+(monthsOffset), 1, 0, 0, 0);
-            _endDate = new Date(_currDate.getFullYear(), _currDate.getMonth()+(monthsOffset)+1, 0, 23, 59, 59);
+            _startDate = new Date(_startDate.getFullYear(), _startDate.getMonth()+(monthsOffset), 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+            _endDate = new Date(_endDate.getFullYear(), _endDate.getMonth()+(monthsOffset)+1, 0, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
             return this;
         },
         /*
@@ -148,32 +188,34 @@ gsDate = (function() {
             _endDate = new Date(_currDate.getFullYear(), _currDate.getMonth(), _currDate.getDate(), 23, 59, 59);
             return this;
         },
-        offddYear = function( yearsOffset ) {
-            if (yearsOffset == undefined || typeof yearsOffset != 'number') {
-                yearsOffset = 0;
-            }
-            _startDate = new Date(_currDate.getFullYear()+(yearsOffset), 0, 1, 0, 0, 0);
-            _endDate = new Date(_currDate.getFullYear()+(yearsOffset), 11, 31, 23, 59, 59);
-            return returnDates();  
+        _offddYear = function( yearsOffset ) {
+            _startDate = new Date(_startDate.getFullYear()+(yearsOffset), 0, 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+            _endDate = new Date(_endDate.getFullYear()+(yearsOffset), 11, 31, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+            return this;
         }
-        ddQuarter = function( quartersOffset ) {
-            var quarterMonth = Math.floor(_currDate.getMonth()/3) * 3;
-            if (quartersOffset == undefined || typeof quartersOffset != 'number') {
-                quartersOffset = 0;
+        _offddQuarter = function(quartersOffset) {
+            var quarterMonth = Math.floor(_startDate.getMonth()/3) * 3;
+            _startDate = new Date(_startDate.getFullYear(), quarterMonth+(quartersOffset*3), 1, _startDate.getHours(), _startDate.getMinutes(), _startDate.getSeconds());
+            _endDate = new Date(_endDate.getFullYear(), quarterMonth+3+(quartersOffset*3), 0, _endDate.getHours(), _endDate.getMinutes(), _endDate.getSeconds());
+            return this;
+        },
+        holidays = function( country ) {
+            var days = [],
+                date,
+                calendar;
+            if ( _holidays.hasOwnProperty(country) ) {
+                calendar = _holidays[country];
+            } else {
+                calendar = _holidays['us'];
             }
-            _startDate = new Date(_currDate.getFullYear(), quarterMonth+(quartersOffset*3), 1, 0, 0, 0);
-            _endDate = new Date(_currDate.getFullYear(), quarterMonth+3+(quartersOffset*3), 0, 23, 59, 59);
 
-            return returnDates();
-        },
-        offddQuarter = function(quartersOffset) {
-            return ddQuarter(quartersOffset);
-        },
-        last4Qtr = function() {
-            var quarterMonth = Math.floor(_currDate.getMonth()/3) * 3;
-            _startDate = new Date(_currDate.getFullYear(), quarterMonth-9, 1, 0, 0, 0);
-            _endDate = new Date(_currDate.getFullYear(), quarterMonth+3, 0, 23, 59, 59);
-            return returnDates();
+            for (var day in calendar) {
+                date = new Date(_startDate.getFullYear(), calendar[day][0], calendar[day][1] );
+                console.log(date);
+                days.push(calendar[day]);
+            }
+
+            return days;
         },
         _NaNException = function(message) {
             this.message = 'Amount provided is not of the expected type (NaN)';
@@ -190,9 +232,9 @@ gsDate = (function() {
         single: setSingle,
         // 2nd section: update
         setDate: setDate,
-        startOn: function() {},
-        endOn: function() {},
         offset: offset,
+        beginOfDay: bod,
+        endOfDay: eod,
         // 3rd section: retrieve data
         getMiliseconds:function() {},
         getSeconds:function() {},
@@ -203,25 +245,8 @@ gsDate = (function() {
         getMonths:function() {},
         getYears:function() {},
         getQuarters:function() {},
+        getHolidays: holidays,
         // 4th section: output data
-        output: returnFnc,
-
-        //TODO:
-        // firstDay(),
-        // lastDay(),
-        // weekOfYear()
-        // multiple dates
-        // single date functions, just like week of year, bicentenary year, holidays
-        
-        // offsetWeek: ddWeek,
-        // getCurrentMonth: ddMonth,
-        // getCurrentYear: ddYear,
-        // getCurrentQuarter: ddQuarter,
-        // getYearToDate: yearToDate,
-        // offsetWeeks: offddWeek,
-        // offsetMonths: offddMonth,
-        // offsetYears: offddYear,
-        // offsetQuarters: offddQuarter,
-        // last4Quarters: last4Qtr
+        output: returnFnc
     }
 })();
